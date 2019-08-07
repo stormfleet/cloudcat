caa9a2b75ccc69fbac4bb1a35c151bbb6a31b5be
stormfleet <48258298+stormfleet@users.noreply.github.com>
2019-08-07 08:48:25 +0000

    Refactored old code for the hashcat execution

--- cloudcat2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import ansible_runner
-import os
-
-cwd = os.getcwd()
-ansible_runner.run(playbook=cwd + '/test-yaml.yml', cmdline='--vault-id @prompt', extravars={'foo':'one','bar':'two','baz':'three'})
