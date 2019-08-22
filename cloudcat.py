9c207e135ae33644b2c0cda3df1b9871694a6c89
stormfleet <48259298+stormfleet@users.noreply.github.com>
2019-08-22 19:14:23 +0000

    Broke python script...

--- cloudcat.py
+++ cloudcat.py
@@ -20,11 +20,11 @@ CloudCat - The cloud-based password cracker.
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t","--type",help="Size of the instance to use. From cheapest to most expensive: p3.2xlarge, p3.8xlarge and p3.16xlarge.",choices=['p3.2xlarge','p3.8xlarge','p3.16xlarge']required=True)
+    parser.add_argument("-t","--type",help="Size of the instance to use. From cheapest to most expensive: p3.2xlarge, p3.8xlarge and p3.16xlarge.",choices=['p3.2xlarge','p3.8xlarge','p3.16xlarge'],required=True)
     parser.add_argument("-f","--file",help="File containing hashes to crack",required=True)
     parser.add_argument("-m","--mode",help="Hashtype cracking mode you want to use",required=True)
     parser.add_argument("-i","--identity",help="SSH identity on AWS to use (only select this if you're sure you have the correct key!)")
-    parser.add_argument("-r","--region",help="Which AWS region to create the account in.")
+    parser.add_argument("-l","--length",help="Length of the hash cracking run. Short is just rockyou.txt, medium is rockyou and fav_wordlist, and long is those two and crackstation.txt."
     parser.add_argument("-g","--single",help="Create an Amazon Security Group where only your current public IP address is allowed through the firewall. You will be unable to SSH to the server from anywhere other than here.",action='store_true')
     parser.add_argument("-o","--double",help="Create an Amazon Security Group where your current pulic IP address and one other public IP address is allowed through the firewall. This second location should be somewhere you always have access to (e.g. home, office).")
     parser.add_argument("-d","--destroy",help="Destroy CloudCat AWS P3.X instances.",action='store_true')
@@ -32,9 +32,9 @@ def parse_args():
 
 def main():
     args = parse_args()
-    shutil.copyfile(args.file, 'roles/taskcat/files/.')
+    shutil.copyfile(args.file, 'roles/taskcat/files/hashes.txt')
     create = ['ansible-playbook', 'cloudcat-create.yml', '-e']
-    ops = ["type={} hashmode={} ssh_key={} identity={} oneip={} length={} guestip={}".format(args.type,args.mode,args.identity,args.single,args.length,args.double]
+    ops = ["type={} hashmode={} ssh_key={} identity={} oneip={} length={} guestip={}".format(args.type,args.mode,args.identity,args.single,args.length,args.double)]
     if args.destroy:
         destroy = ['ansible-playbook', 'cloudcat-destroy.yml']
         subprocess.call(arrayd)
