7f7be74fd20824c8602fcf9052e165dc618845f3
stormfleet <48259298+stormfleet@users.noreply.github.com>
2019-08-24 18:55:41 +0000

    Replace identity switch with aws-identity, add ssh-key option to segregate options.

--- cloudcat.py
+++ cloudcat.py
@@ -27,7 +27,8 @@ def parse_args():
     parser.add_argument("-t","--type",help="Size of the instance to use. From cheapest to most expensive: p3.2xlarge, p3.8xlarge and p3.16xlarge.",choices=['p3.2xlarge','p3.8xlarge','p3.16xlarge'], action="store")
     parser.add_argument("-f","--file",help="File containing hashes to crack", action="store")
     parser.add_argument("-m","--mode",help="Hashtype cracking mode you want to use", action="store")
-    parser.add_argument("-i","--identity",help="SSH identity on AWS to use (only select this if you're sure you have the correct key!)", action="store")
+    parser.add_argument("-i","--identity",help="AWS identity to use (only select this if you're sure you have the correct key!)", action="store")
+    parser.add_argument("-k","--ssh-key",dest="sshkey",help="SSH key-file name. Used to connect to created CloudCat instances to conduct tasks and launch Hashcat.", action="store")
     parser.add_argument("-l","--length",help="Length of the hash cracking run. Short is just rockyou.txt, medium is rockyou and fav_wordlist, and long is those two and crackstation.txt.", action="store")
     parser.add_argument("-s","--single",help="Create an Amazon Security Group where only your current public IP address is allowed through the firewall. You will be unable to SSH to the server from anywhere other than here.", action="store_true")
     parser.add_argument("--double",help="Create an Amazon Security Group where your current pulic IP address and one other public IP address is allowed through the firewall. This second location should be somewhere you always have access to (e.g. home, office).", action="store")
@@ -96,14 +97,14 @@ def main():
         destroy = ['ansible-playbook', 'cloudcat-destroy.yml']
         subprocess.call(destroy)
         sys.exit(0)
-    if args.type and (args.identity is None or args.file is None or args.mode is None or args.length is None):
+    if args.type and (args.sshkey is None or args.identity is None or args.file is None or args.mode is None or args.length is None):
         parser = argparse.ArgumentParser()
         parser.error("You have defined an instance type but not additional parameters to create the instance. Exiting...")
         sys.exit(0)
     if args.type:
-        #shutil.copyfile(args.file, 'roles/taskcat/files/hashes.txt')
+        shutil.copyfile(args.file, 'roles/taskcat/files/hashes.txt')
         create = ['ansible-playbook', 'cloudcat-create.yml', '-e']
-        ops = ["type={} hashmode={} ssh_key={} identity={} oneip={} length={}".format(args.type,args.mode,args.identity,args.identity,args.single,args.length)]
+        ops = ["type={} hashmode={} ssh_key={} identity={} oneip={} length={}".format(args.type,args.mode,args.sshkey,args.identity,args.single,args.length)]
         if args.double:
             ops.append(['-e double={}'.format(args.double)])
             return
