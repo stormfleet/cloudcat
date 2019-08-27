74cafe4cbc1f8f6a2af179c4f905aec56d18fe7f
stormfleet <48259298+stormfleet@users.noreply.github.com>
2019-08-27 22:38:36 +0100

    Add in ansible-runner for posterity.

--- cloudcat.py
+++ cloudcat.py
@@ -114,6 +114,22 @@ def main():
         print(create)
         print("Creating CloudCat cracking instance with " + args.type + " accelerated computing instance.")
         subprocess.call(create)
+	
+#!/usr/bin/python3
+#import ansible_runner
+#import os
+#cwd = os.getcwd()
+#ansible_runner.run(playbook=cwd + '/test-yaml.yml', cmdline='--vault-id @prompt', extravars={'foo':'one','bar':'two','baz':'three'})
+#
+#!/usr/bin/python3
+#import ansible_runner
+#r = ansible_runner.run(private_data_dir='/tmp/demo', playbook='/home/user/dev/cloudcat/creation.yml')
+#print("{}: {}".format(r.status, r.rc))
+# successful: 0
+#for each_host_event in r.events:
+#    print(each_host_event['event'])
+#print("Final status:")
+#print(r.stats)
 
 if __name__ == "__main__":
     banner()
