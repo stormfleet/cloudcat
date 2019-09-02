ae22ca4d4089fd7693e9d08e0ba6528319b191b8
stormfleet <48259298+stormfleet@users.noreply.github.com>
2019-09-02 18:27:31 +0000

    Remove setup.sh, create and store artifacts from previous commits. Might be useful in future.

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "[+] Performing CloudCat setup."
-echo "[+] Installing required packages..."
-sudo apt install ansible
-pip install botocore boto3 ansible --user
-echo "[+] Creating external_vars.yml file"
-ansible-vault create external_vars.yml
